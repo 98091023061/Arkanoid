af2dc26ea455f42d5c95722650fa1c6bde350f32
Joel Daniel <beymaxjd@gmail.com>
2023-12-14 12:28:04 +0700

    ParametroRestSpread

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -87,7 +87,36 @@ console.log(dog);
 dog.ladrar();
 }
 
-objetosLiterales();
+// objetosLiterales();
+
+function ParametroRestSpread(){
+
+    function sumar (a,b,...c){
+        let resultado = a +b;
+        c.forEach(function(n){
+            resultado+=n;
+        });
+
+        return resultado;
+    }
+    console.log(sumar(1,2));
+    console.log(sumar(1,2,3,4,5));
+    console.log(sumar(1,2,3,4,5,6,7));
+
+    const arr1 = [1,2,3,4,5];
+    const arr2 = [6,7,8,9,0];
+
+    console.log(arr1,arr2);
+    // Spread Operator
+    const arr3 = [...arr1,...arr2];
+
+    console.log(arr3);
+
+}
+
+ParametroRestSpread();
+
+
 
 
 
